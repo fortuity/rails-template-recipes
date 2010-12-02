06d70aa218bd4c4de55aef6f5fc04de93d470909
Fletcher Nichol <fnichol@nichol.ca>
2010-12-02 16:28:47 -0700

    Attempt the inject_into_file approach.

--- cucumber_extras.rb
+++ cucumber_extras.rb
@@ -2,17 +2,19 @@ say_recipe 'Cucumber extras'
 
 after_bundler do
   # Add autotest runner erb opts
-  gsub_file "config/cucumber.yml", /^(std_opts = .* ~@wip)"$/, '\1 ' <<-'OPTS'.gsub(/^ {4}/, '')
-    --tags ~@proposed --color"
-    autotest_opts = "--format pretty --strict --tags ~@proposed --color"
-    autotest_all_opts = "--format #{ENV['CUCUMBER_FORMAT'] || 'progress'} --strict --tags ~@proposed --color #{ENV['CUCUMBER_EXCLUDE']}"
-  OPTS
+  inject_into_file "config/cucumber.yml", :before => '%>' do
+    <<-'OPTS'.gsub(/^ {6}/, '')
+      std_opts += "--tags ~@proposed --color"
+      autotest_opts = "--format pretty --strict --tags ~@proposed --color"
+      autotest_all_opts = "--format #{ENV['CUCUMBER_FORMAT'] || 'progress'} --strict --tags ~@proposed --color #{ENV['CUCUMBER_EXCLUDE']}"
+    OPTS
+  end
 
   # Add autotest runner formats
-  gsub_file "config/cucumber.yml", /^(rerun: .* ~@wip)$/, '\1 ' <<-'YAML'.gsub(/^ {4}/, '')
-    --tags ~@proposed
-    autotest: <%= autotest_opts %> features
-    autotest-all: <%= autotest_all_opts %> features
-  YAML
+#  gsub_file "config/cucumber.yml", /^(rerun: .* ~@wip)$/, '\1 ' <<-'YAML'.gsub(/^ {4}/, '')
+#    --tags ~@proposed
+#    autotest: <%= autotest_opts %> features
+#    autotest-all: <%= autotest_all_opts %> features
+#  YAML
 end
 
