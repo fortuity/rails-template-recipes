b9dba6d2c5be884dcd5e39a8970a1a833eb697fa
Fletcher Nichol <fnichol@nichol.ca>
2010-12-06 15:56:05 -0700

    Add autotest-fsevent if rails project is created with a mac.

--- autotest.rb
+++ autotest.rb
@@ -3,3 +3,9 @@ say_recipe 'autotest'
 gem 'autotest', :group => :test
 gem 'autotest-growl', :group => :test
 
+# if rails project is being generated from a mac, then add a mac bundler group
+# and exclude the group when bundler'ing on other platforms
+if RUBY_PLATFORM =~ /darwin/
+  gem 'autotest-fsevent', :group => :mac
+end
+
