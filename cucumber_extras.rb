826ecedb630ef5d3458b76a51cd938e1130da117
Fletcher Nichol <fnichol@nichol.ca>
2010-12-06 12:55:44 -0700

    Fix up cucumber.yml injections (hot beef).

--- cucumber_extras.rb
+++ cucumber_extras.rb
@@ -2,13 +2,12 @@ say_recipe 'Cucumber extras'
 
 after_bundler do
   # Add autotest runner erb opts
-  inject_into_file "config/cucumber.yml", :before => '%>\n' do
-    <<-'OPTS'.gsub(/^ {6}/, '')
-      std_opts += "--tags ~@proposed --color"
-      autotest_opts = "--format pretty --strict --tags ~@proposed --color"
-      autotest_all_opts = "--format #{ENV['CUCUMBER_FORMAT'] || 'progress'} --strict --tags ~@proposed --color #{ENV['CUCUMBER_EXCLUDE']}"
-    OPTS
-  end
+  gsub_file "config/cucumber.yml", /^(std_opts = .*wip")$/, '\1' << <<-'OPTS'.gsub(/^ {4}/, '')
+
+    std_opts += " --tags ~@proposed --color"
+    autotest_opts = "--format pretty --strict --tags ~@proposed --color"
+    autotest_all_opts = "--format #{ENV['CUCUMBER_FORMAT'] || 'progress'} --strict --tags ~@proposed --color #{ENV['CUCUMBER_EXCLUDE']}"
+  OPTS
 
   # Add autotest runner formats
   gsub_file "config/cucumber.yml", /^(rerun: .* ~@wip)$/, '\1 --tags ~@proposed' 
