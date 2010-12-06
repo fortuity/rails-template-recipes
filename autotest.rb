83deee0fd3661152a9adc9fb1cf5b0f324027a96
Fletcher Nichol <fnichol@nichol.ca>
2010-12-06 16:04:39 -0700

    Add .autotest with mac detection for autotest-fsevent.

--- autotest.rb
+++ autotest.rb
@@ -9,3 +9,25 @@ if RUBY_PLATFORM =~ /darwin/
   gem 'autotest-fsevent', :group => :mac
 end
 
+after_bundler do
+  create_file '.autotest' do
+    <<-'AUTOTEST'.gsub(/^ {6}/, '')
+      require 'autotest/growl'
+      if RUBY_PLATFORM =~ /-darwin/
+        begin
+          require 'autotest/fsevent'
+        rescue LoadError
+          puts "== autotest-fsevent gem will improve performance on Mac OS X"
+          puts "== to use, just: gem install autotest-fsevent"
+        end
+      end
+       
+      Autotest.add_hook :initialize do |autotest|
+        %w{.git .svn .hg .DS_Store ._* vendor tmp log doc}.each do |exception|
+          autotest.add_exception(exception)
+        end
+      end
+    AUTOTEST
+  end
+end
+
