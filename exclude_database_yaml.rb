c36fd6591f478d46a840fa2f2f486b89aba4887e
Fletcher Nichol <fnichol@nichol.ca>
2010-12-16 16:40:56 -0700

    Streamline exclude_database_yaml recipe.

--- exclude_database_yaml.rb
+++ exclude_database_yaml.rb
@@ -1,8 +1,5 @@
 say_recipe 'Exclude database.yml from version control'
 
-copy_file "config/database.yml", "config/database.sample.yml"
-remove_file "config/database.yml"
-
 create_file 'lib/tasks/init_database_yaml.rake' do
   <<-'RAKE'.gsub(/^ {6}/, '')
     namespace :init do
@@ -22,4 +19,14 @@ create_file 'lib/tasks/init_database_yaml.rake' do
     # Add namespaced tasks to default :init task
     Rake::Task["init"].enhance { Rake::Task["init:database_yaml"].invoke }
   RAKE
+end
+
+# wait until all generators have had a chance to touch config/database.yml
+after_bundler do
+  say_wizard "Adding Java platform detection in database.yml"
+  gsub_file "config/database.yml", /^(\s*adapter:) (.+)$/,
+    %q{\1 <%= RUBY_PLATFORM =~ /java/ ? '\2' : '\2' %>}
+
+  say_wizard "Creating config/database.sample.yml for version control"
+  FileUtils.cp "config/database.yml", "config/database.sample.yml"
 end
