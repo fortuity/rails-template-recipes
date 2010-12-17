aee9a977ea240fa1a0af8a1b719fdca54f242717
Fletcher Nichol <fnichol@nichol.ca>
2010-12-16 20:30:59 -0700

    Add lib/tasks/auto.rake.

--- autotest.rb
+++ autotest.rb
@@ -9,6 +9,48 @@ if RUBY_PLATFORM =~ /darwin/
   gem 'autotest-fsevent', :group => :mac
 end
 
+create_file 'lib/tasks/auto.rake' do
+  <<-'RAKE'.gsub(/^ {4}/, '')
+    namespace :auto do
+      desc 'Runs autotest on cucumber and rspec tests'
+      task :test do
+        gem 'test-unit', '1.2.3' if RUBY_VERSION.to_f >= 1.9
+        ENV['RSPEC'] = 'true'       # allows autotest to discover rspec
+        ENV['AUTOTEST'] = 'true'    # allows autotest to run w/ color on linux
+        ENV['AUTOFEATURE'] = 'true' # allows autotest to discover cucumber
+        system((RUBY_PLATFORM =~ /mswin|mingw/ ? 'autotest.bat' : 'autotest'), *ARGV) ||
+          $stderr.puts("Unable to find autotest.  Please install ZenTest or fix your PATH")
+      end
+
+      desc 'Runs autotest on only rspec tests'
+      task :spec do
+        gem 'test-unit', '1.2.3' if RUBY_VERSION.to_f >= 1.9
+        ENV['RSPEC'] = 'true'       # allows autotest to discover rspec
+        ENV['AUTOTEST'] = 'true'    # allows autotest to run w/ color on linux
+        ENV['AUTOFEATURE'] = 'false' # allows autotest to discover cucumber
+        system((RUBY_PLATFORM =~ /mswin|mingw/ ? 'autotest.bat' : 'autotest'), *ARGV) ||
+          $stderr.puts("Unable to find autotest.  Please install ZenTest or fix your PATH")
+      end
+
+      desc 'Runs autotest on only cucumber tests'
+      task :cucumber do
+        gem 'test-unit', '1.2.3' if RUBY_VERSION.to_f >= 1.9
+        ENV['RSPEC'] = 'false'       # allows autotest to discover rspec
+        ENV['AUTOTEST'] = 'true'    # allows autotest to run w/ color on linux
+        ENV['AUTOFEATURE'] = 'true' # allows autotest to discover cucumber
+        system((RUBY_PLATFORM =~ /mswin|mingw/ ? 'autotest.bat' : 'autotest'), *ARGV) ||
+          $stderr.puts("Unable to find autotest.  Please install ZenTest or fix your PATH")
+      end
+
+      desc 'Alias for auto:test'
+      task :all => 'auto:test'
+    end
+
+    desc 'Autotest'
+    task :auto => 'auto:test'
+  RAKE
+end
+
 after_bundler do
   create_file '.autotest' do
     <<-'AUTOTEST'.gsub(/^ {6}/, '')
