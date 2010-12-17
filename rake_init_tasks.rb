afc87556b7e047bbd286c680e908067fe81865a4
Fletcher Nichol <fnichol@nichol.ca>
2010-12-16 21:04:13 -0700

    Add "db:test:prepare" to rake init.

--- rake_init_tasks.rb
+++ rake_init_tasks.rb
@@ -8,7 +8,7 @@ create_file 'lib/tasks/init.rake' do
     end
 
     # Add namespaced tasks to default :init task
-    db_tasks = %w{ tmp:create db:schema:dump db:setup }
+    db_tasks = %w{ tmp:create db:schema:dump db:setup db:test:prepare }
     Rake::Task["init"].enhance do
       db_tasks.each { |t| Rake::Task[t].invoke }
     end
