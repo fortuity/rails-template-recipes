a3e019e6455c219f13b825c40bdd282b78cde1a0
Fletcher Nichol <fnichol@nichol.ca>
2010-12-22 14:47:12 -0700

    Add jruby support for activerecord in Gemfile.

--- activerecord_extras.rb
+++ activerecord_extras.rb
@@ -1,3 +1,11 @@
 say_recipe 'ActiveRecord extras'
 
-gem 'mysql', :group => :production
+gem 'mysql',
+  :group => :production, :platforms => :ruby
+gem 'activerecord-jdbcmysql-adapter',
+  :group => :production, :platforms => :jruby, :require => false
+
+gem 'sqlite3-ruby',
+  :group => [:development, :test], :platforms => :ruby
+gem 'activerecord-jdbcsqlite3-adapter',
+  :group => [:development, :test], :platforms => :jruby, :require => false
