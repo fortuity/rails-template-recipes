18b5d982555fa001ff5d01c7da9247e9db4dcec0
fnichol <fnichol@nichol.ca>
2011-02-16 11:58:46 -0700

    Conditionally run flashes_partial.erb is erb recipe is executed.

--- flashes_partial.erb.rb
+++ flashes_partial.erb.rb
@@ -1,12 +1,13 @@
-say_recipe 'Flashes partial (ERb)'
+if recipes.include? 'erb'
+  say_recipe 'Flashes partial (ERb)'
 
-create_file 'app/views/shared/_flashes.html.erb' do
-  <<-'FLASHES'.gsub(/^ {4}/, '')
-    <div id="flash">
-      <% flash.each do |key, value| -%>
-        <div id="flash_<%= key %>"><%=h value %></div>
-      <% end -%>
-    </div>
-  FLASHES
+  create_file 'app/views/shared/_flashes.html.erb' do
+    <<-'FLASHES'.gsub(/^ {6}/, '')
+      <div id="flash">
+        <% flash.each do |key, value| -%>
+          <div id="flash_<%= key %>"><%=h value %></div>
+        <% end -%>
+      </div>
+    FLASHES
+  end
 end
-
