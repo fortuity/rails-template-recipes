c5bab3e65959f3d804caf3dacfa6463ee43a3397
Daniel Kehoe <kehoe@fortuity.com>
2011-02-17 14:59:45 -0800

    revert change to cleanup recipe

--- cleanup.rb
+++ cleanup.rb
@@ -2,26 +2,22 @@
 
 say_recipe 'cleanup'
 
-after_bundler do
-  
-  # remove unnecessary files
-  %w{
-    README
-    doc/README_FOR_APP
-    public/index.html
-    public/favicon.ico
-    public/images/rails.png
-  }.each { |file| remove_file file }
+# remove unnecessary files
+%w{
+  README
+  doc/README_FOR_APP
+  public/index.html
+  public/favicon.ico
+  public/images/rails.png
+}.each { |file| remove_file file }
 
-  # remove commented lines from Gemfile
-  # thanks to https://github.com/perfectline/template-bucket/blob/master/cleanup.rb
-  gsub_file "Gemfile", /#.*\n/, "\n"
-  gsub_file "Gemfile", /\n+/, "\n"
+# remove commented lines from Gemfile
+# thanks to https://github.com/perfectline/template-bucket/blob/master/cleanup.rb
+gsub_file "Gemfile", /#.*\n/, "\n"
+gsub_file "Gemfile", /\n+/, "\n"
 
-  if extra_recipes.include? 'git'
-    say_wizard "commiting deletes of unneeded files to git"
-    git :add => '.'
-    git :commit => "-am 'Removed unnecessary files left over from initial app generation.'"
-  end
-  
+if extra_recipes.include? 'git'
+  say_wizard "commiting deletes of unneeded files to git"
+  git :add => '.'
+  git :commit => "-am 'Removed unnecessary files left over from initial app generation.'"
 end
