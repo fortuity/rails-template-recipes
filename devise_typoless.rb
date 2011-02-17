309eb4d5fc109c47871a99cca05be1f83ceb6e80
Daniel Kehoe <kehoe@fortuity.com>
2011-02-16 20:17:13 -0800

    remove previous diagnostics, add recipe for Devise (like the RailsWizard recipe but without a typo)

---
+++ devise_typoless.rb
@@ -0,0 +1,35 @@
+# >--------------------------------[ Devise ]---------------------------------<
+
+# Utilize Devise for authentication, automatically configured for your selected ORM.
+say_recipe 'Devise'
+
+gem 'devise'
+
+after_bundler do
+  generate 'devise:install'
+
+  # the case statement is cleaner but fails evaluating template['orm']
+  # case template['orm']
+  #   when 'mongo_mapper'
+  #     gem 'mm-devise'
+  #     gsub_file 'config/intializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongo_mapper_active_model'
+  #   when 'mongoid'
+  #     gsub_file 'config/intializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongoid'
+  #   when 'active_record'
+  #     # Nothing to do
+  # end
+  
+  if recipe_list.include? 'mongo_mapper'
+    gem 'mm-devise'
+    gsub_file 'config/intializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongo_mapper_active_model'
+  elsif recipe_list.include? 'mongoid'
+    gsub_file 'config/intializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongoid'
+  elsif recipe_list.include? 'active_record'
+    # Nothing to do
+  else
+    # Nothing to do
+  end
+  
+  generate 'devise user'
+  
+end
