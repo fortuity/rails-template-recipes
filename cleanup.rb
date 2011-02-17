76436b09e47889d0beadae9a9bfc9d13d92217ae
Daniel Kehoe <kehoe@fortuity.com>
2011-02-17 14:58:24 -0800

    run cleanup recipe after bundler

--- cleanup.rb
+++ cleanup.rb
@@ -2,22 +2,26 @@
 
 say_recipe 'cleanup'
 
-# remove unnecessary files
-%w{
-  README
-  doc/README_FOR_APP
-  public/index.html
-  public/favicon.ico
-  public/images/rails.png
-}.each { |file| remove_file file }
+after_bundler do
+  
+  # remove unnecessary files
+  %w{
+    README
+    doc/README_FOR_APP
+    public/index.html
+    public/favicon.ico
+    public/images/rails.png
+  }.each { |file| remove_file file }
 
-# remove commented lines from Gemfile
-# thanks to https://github.com/perfectline/template-bucket/blob/master/cleanup.rb
-gsub_file "Gemfile", /#.*\n/, "\n"
-gsub_file "Gemfile", /\n+/, "\n"
+  # remove commented lines from Gemfile
+  # thanks to https://github.com/perfectline/template-bucket/blob/master/cleanup.rb
+  gsub_file "Gemfile", /#.*\n/, "\n"
+  gsub_file "Gemfile", /\n+/, "\n"
 
-if extra_recipes.include? 'git'
-  say_wizard "commiting deletes of unneeded files to git"
-  git :add => '.'
-  git :commit => "-am 'Removed unnecessary files left over from initial app generation.'"
+  if extra_recipes.include? 'git'
+    say_wizard "commiting deletes of unneeded files to git"
+    git :add => '.'
+    git :commit => "-am 'Removed unnecessary files left over from initial app generation.'"
+  end
+  
 end
