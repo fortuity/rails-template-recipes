26b6c3d6850642538606575141b7a9734773a659
Daniel Kehoe <kehoe@fortuity.com>
2011-02-16 19:53:02 -0800

    diagnosing anomaly

--- test.rb
+++ test.rb
@@ -1,3 +1,11 @@
 # >--------------------------------[ Test of Conditional ]---------------------------------<
 
-say_recipe "template['orm'] is blank"
+say_wizard "begin test"
+
+if recipe_list.include? 'haml'
+  say_wizard 'recipe list includes Haml'
+end
+
+say_wizard "template['orm'] is #{template['orm']}"
+
+say_wizard "test complete"
