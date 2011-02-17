4fa097325afebb9cde434aba6359f481564666e3
Daniel Kehoe <kehoe@fortuity.com>
2011-02-16 17:11:16 -0800

    diagnosing

--- devise_typoless.rb
+++ devise_typoless.rb
@@ -8,8 +8,6 @@ gem 'devise'
 after_bundler do
   generate 'devise:install'
 
-  say_recipe "template['orm'] is #{template['orm']}"
-
   gsub_file 'config/initializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongoid'
 
   generate 'devise user'
