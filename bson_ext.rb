709d93345f9df096e0c3954bb7106e67feb460e3
Daniel Kehoe <kehoe@fortuity.com>
2011-02-17 15:52:50 -0800

    make installation of bson_ext gem contingent on use of mongoid

--- bson_ext.rb
+++ bson_ext.rb
@@ -1,6 +1,12 @@
 # >--------------------------------[ bson_ext ]--------------------------------<
 
-# Add bson_ext gem for use with Mongoid
-say_recipe 'bson_ext'
+if recipe_list.include? 'mongoid'
+  
+  # Add bson_ext gem for use with Mongoid
+  say_recipe 'bson_ext'
 
-gem 'bson_ext'
+  gem 'bson_ext'
+  
+end
+
+
