e5102d801901ad9d30500f72b69b650eda64929b
Daniel Kehoe <kehoe@fortuity.com>
2011-02-16 17:07:25 -0800

    can't get the template value? (diagnosing)

--- devise_typoless.rb
+++ devise_typoless.rb
@@ -8,15 +8,10 @@ gem 'devise'
 after_bundler do
   generate 'devise:install'
 
-  case template['orm']
-    when 'mongo_mapper'
-      gem 'mm-devise'
-      gsub_file 'config/initializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongo_mapper_active_model'
-    when 'mongoid'
-      gsub_file 'config/initializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongoid'
-    when 'active_record'
-      # Nothing to do
-      
-    generate 'devise user'
-  end
+  say_recipe "template['orm'] is #{template['orm']}"
+
+  gsub_file 'config/initializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongoid'
+
+  generate 'devise user'
+
 end
