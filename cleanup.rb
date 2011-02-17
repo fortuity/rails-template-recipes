a320857da785c6cd642f7ec7f0566b96a1de70a2
Daniel Kehoe <kehoe@fortuity.com>
2011-02-17 14:53:55 -0800

    modified 'cleanup' recipe

--- cleanup.rb
+++ cleanup.rb
@@ -1,6 +1,8 @@
+# >--------------------------------[ cleanup ]--------------------------------<
+
 say_recipe 'cleanup'
 
-# remove default cruft
+# remove unnecessary files
 %w{
   README
   doc/README_FOR_APP
@@ -14,3 +16,8 @@ say_recipe 'cleanup'
 gsub_file "Gemfile", /#.*\n/, "\n"
 gsub_file "Gemfile", /\n+/, "\n"
 
+if extra_recipes.include? 'git'
+  say_wizard "commiting deletes of unneeded files to git"
+  git :add => '.'
+  git :commit => "-am 'Removed unnecessary files left over from initial app generation.'"
+end
