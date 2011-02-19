e27508ffeda04b7bbd09867762a284278ca506de
Daniel Kehoe <kehoe@fortuity.com>
2011-02-18 20:01:50 -0800

    make mongoid cleanup recipe contingent on presence of mongoid

--- mongoid_cleanup.rb
+++ mongoid_cleanup.rb
@@ -2,21 +2,25 @@
 
 say_recipe 'Mongoid cleanup'
 
-# modifying 'config/application.rb' file to remove ActiveRecord dependency
-gsub_file 'config/application.rb', /require 'rails\/all'/ do
-<<-RUBY
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'active_resource/railtie'
-require 'rails/test_unit/railtie'
-RUBY
-end
+if recipe_list.include? 'mongoid'
 
-# remove unnecessary 'config/database.yml' file
-remove_file 'config/database.yml'
+  # modifying 'config/application.rb' file to remove ActiveRecord dependency
+  gsub_file 'config/application.rb', /require 'rails\/all'/ do
+  <<-RUBY
+  require 'action_controller/railtie'
+  require 'action_mailer/railtie'
+  require 'active_resource/railtie'
+  require 'rails/test_unit/railtie'
+  RUBY
+  end
 
-if extra_recipes.include? 'git'
-  say_wizard "commiting changes to git"
-  git :add => '.'
-  git :commit => "-am 'Fix config/application.rb file to remove ActiveRecord dependency.'"
+  # remove unnecessary 'config/database.yml' file
+  remove_file 'config/database.yml'
+
+  if extra_recipes.include? 'git'
+    say_wizard "commiting changes to git"
+    git :add => '.'
+    git :commit => "-am 'Fix config/application.rb file to remove ActiveRecord dependency.'"
+  end
+
 end
