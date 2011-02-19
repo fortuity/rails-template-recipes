2c95c15467cb2321cda6583b08bbac82a9c6ef8d
Daniel Kehoe <kehoe@fortuity.com>
2011-02-18 20:04:28 -0800

    add add_user_name recipe

---
+++ add_user_name.rb
@@ -0,0 +1,22 @@
+# >--------------------------------[ add_user_name ]--------------------------------<
+
+say_recipe 'add_user_name'
+
+# adding a 'name' attribute to the User model
+if recipe_list.include? 'mongoid'
+  gsub_file 'app/models/user.rb', /end/ do
+  <<-RUBY
+    field :name
+    validates_presence_of :name
+    validates_uniqueness_of :name, :email, :case_sensitive => false
+    attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  end
+  RUBY
+  end
+end
+
+if extra_recipes.include? 'git'
+  say_wizard "commiting changes to git"
+  git :add => '.'
+  git :commit => "-am 'Add a name attribute to the User model.'"
+end
