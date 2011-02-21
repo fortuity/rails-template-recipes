d443aaf24af4d84fd1c539a0f47398a8042c1aca
Daniel Kehoe <kehoe@fortuity.com>
2011-02-20 20:54:38 -0800

    tweak indenting

--- seed_database.rb
+++ seed_database.rb
@@ -10,12 +10,12 @@ after_bundler do
       # create a default user
       say_wizard "creating a default user"
       append_file 'db/seeds.rb' do <<-FILE
-      puts 'EMPTY THE MONGODB DATABASE'
-      Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
-      puts 'SETTING UP DEFAULT USER LOGIN'
-      user = User.create! :name => 'First User', :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
-      puts 'New user created: ' << user.name
-      FILE
+puts 'EMPTY THE MONGODB DATABASE'
+Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First User', :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user.name
+FILE
       end
     end
   
