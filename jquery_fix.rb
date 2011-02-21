52479d3d839aba4620730e586806f5a013be1f75
Daniel Kehoe <kehoe@fortuity.com>
2011-02-21 15:07:32 -0800

    add recipe jquery_fix

---
+++ jquery_fix.rb
@@ -0,0 +1,7 @@
+# >--------------------------------[ jquery_fix ]--------------------------------<
+
+say_recipe 'jQuery Fix'
+
+# see https://github.com/intridea/rails_wizard/issues#issue/27
+# the standard RailsWizard jQuery recipe has an error that must be fixed with this:
+gsub_file "config/application.rb", /config.action_view.javascript_expansions[:defaults] = %w()\n/, ""
