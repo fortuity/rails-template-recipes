0d055c262b4253f5848c66af4125e9a931bc0727
Daniel Kehoe <kehoe@fortuity.com>
2011-02-21 16:36:43 -0800

    try a fix

--- jquery_fix.rb
+++ jquery_fix.rb
@@ -6,10 +6,10 @@ after_bundler do
   say_wizard 'applying jQuery fix'
   # see https://github.com/intridea/rails_wizard/issues#issue/27
   # the standard RailsWizard jQuery recipe has an error that must be fixed with this:
-  gsub_file "config/application.rb", /config.action_view.javascript_expansions[:defaults] = \%w()/, ""
+  gsub_file "config/application.rb", /config.action_view.javascript_expansions\[:defaults\] = %w()/, ""
   # a little test
   gsub_file "config/application.rb", /# Configure/, "# (test) Configure"
   gsub_file "config/application.rb", /config.action_view.javascript_expansions/, "foobar"
   gsub_file "config/application.rb", /foobar\[:defaults/, "foobar"
-  gsub_file "config/application.rb", /foobar[/, "foobar"
+  gsub_file "config/application.rb", /foobar\[/, "foobar"
 end
