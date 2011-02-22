5858cdcadd9b1702f03a874c6cde126c7b54e897
Daniel Kehoe <kehoe@fortuity.com>
2011-02-21 16:06:09 -0800

    after_bundler action for jquery_fix

--- jquery_fix.rb
+++ jquery_fix.rb
@@ -2,6 +2,8 @@
 
 say_recipe 'jQuery Fix'
 
-# see https://github.com/intridea/rails_wizard/issues#issue/27
-# the standard RailsWizard jQuery recipe has an error that must be fixed with this:
-gsub_file "config/application.rb", /config.action_view.javascript_expansions[:defaults] = %w()\n/, ""
+after_bundler do
+  # see https://github.com/intridea/rails_wizard/issues#issue/27
+  # the standard RailsWizard jQuery recipe has an error that must be fixed with this:
+  gsub_file "config/application.rb", /config.action_view.javascript_expansions[:defaults] = %w()\n/, ""
+end
