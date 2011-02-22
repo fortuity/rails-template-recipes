b8c052ce952289b8ebaba6e015bb30a39e351167
Daniel Kehoe <kehoe@fortuity.com>
2011-02-21 17:23:27 -0800

    remove comments from devise_extras recipe

--- devise_extras.rb
+++ devise_extras.rb
@@ -10,17 +10,6 @@ gem 'devise'
 after_bundler do
   generate 'devise:install'
 
-  # the case statement is cleaner but fails evaluating template['orm']
-  # case template['orm']
-  #   when 'mongo_mapper'
-  #     gem 'mm-devise'
-  #     gsub_file 'config/initializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongo_mapper_active_model'
-  #   when 'mongoid'
-  #     gsub_file 'config/initializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongoid'
-  #   when 'active_record'
-  #     # Nothing to do
-  # end
-  
   if recipe_list.include? 'mongo_mapper'
     gem 'mm-devise'
     gsub_file 'config/initializers/devise.rb', 'devise/orm/active_record', 'devise/orm/mongo_mapper_active_model'
