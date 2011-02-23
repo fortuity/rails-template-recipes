7d8da3cb66aae8905bee7032c44369adab324d04
Daniel Kehoe <kehoe@fortuity.com>
2011-02-22 21:23:20 -0800

    update devise_navigation recipe

--- devise_navigation.rb
+++ devise_navigation.rb
@@ -61,12 +61,33 @@ ERB
       end
     end
 
-  end
+    #----------------------------------------------------------------------------
+    # Add navigation links to the default application layout
+    #----------------------------------------------------------------------------
+    if recipe_list.include? 'haml'
+      inject_into_file 'app/views/layouts/application.html.erb', :after => "%body\n" do <<-HAML
+  %ul.hmenu
+    = render 'devise/menu/registration_items'
+    = render 'devise/menu/login_items'
+HAML
+      end
+    else
+      inject_into_file 'app/views/layouts/application.html.erb', :after => "<body>\n" do
+  <<-ERB
+  <ul class="hmenu">
+    <%= render 'devise/menu/registration_items' %>
+    <%= render 'devise/menu/login_items' %>
+  </ul>
+ERB
+      end
+    end
 
-  if extra_recipes.include? 'git'
-    say_wizard "commiting changes to git"
-    git :add => '.'
-    git :commit => "-am 'Add navigation links for Devise.'"
+    if extra_recipes.include? 'git'
+      say_wizard "commiting changes to git"
+      git :add => '.'
+      git :commit => "-am 'Add navigation links for Devise.'"
+    end
+
   end
 
 end
