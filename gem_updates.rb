e03f55ad8b23e6dc89a92dc38812bdcefd047f61
Daniel Kehoe <kehoe@fortuity.com>
2011-02-26 13:06:51 -0800

    remove superfluous  gem_updates recipe

--- gem_updates.rb
+++
@@ -1,7 +0,0 @@
-# >--------------------------------[ gem_updates ]--------------------------------<
-
-# Application template recipe. Check for a newer version here:
-# https://github.com/fortuity/rails-template-recipes/blob/master/gem_updates.rb
-
-say_recipe 'Gem Updates'
-
