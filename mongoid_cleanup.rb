6ec9226a862804aecbed9707050291fa5f16c29a
Daniel Kehoe <kehoe@fortuity.com>
2011-02-28 13:14:32 -0800

    resolve issue 17, change YAML Engine to avoid fail validating database name, thanks Carsten Block

--- mongoid_cleanup.rb
+++ mongoid_cleanup.rb
@@ -23,6 +23,16 @@ if recipe_list.include? 'mongoid'
   # remove unnecessary 'config/database.yml' file
   remove_file 'config/database.yml'
 
+  #----------------------------------------------------------------------------
+  # Change YAML Engine to accommodate Ruby 1.9.2p180 yaml parser problem.
+  # Rubygems 1.5.0 changes the yaml parsing default from syck 
+  # to psych and psych doesn't like the ":<<" in yaml files.
+  # This is a workaround until the next version of Ruby is published.
+  # http://groups.google.com/group/mongoid/browse_thread/thread/9213a17a73d3c422
+  # http://redmine.ruby-lang.org/issues/show/4300
+  #----------------------------------------------------------------------------                         
+  inject_into_file 'config/environment.rb', "\nrequire 'yaml'\nYAML::ENGINE.yamler= 'syck'\n", :after => "require File.expand_path('../application', __FILE__)", :verbose => false
+
   if extra_recipes.include? 'git'
     git :tag => "mongoid_cleanup"
     git :add => '.'
